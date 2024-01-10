c0434213d068e537ea7dc6033412bc6fbdb84a11
Sofie Zomerman <pnl14h7t@Emea.Royalahold.net>
2024-01-10 13:49:37 +0100

    twee namen

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,13 @@
-def likes(team: list) -> str:
-    return True
+def likes(list_names):
+    if len(list_names) == 0:
+        true =  'no one likes this post'
+    elif len(list_names) == 1:
+        true = list_names[0] + ' likes this'
+    else:
+        true = list_names[0] + ' and ' + list_names[1] + ' like this post'
+    return true
+
+
+names = ['Niels', 'Yur']
+print(likes(names))
+
